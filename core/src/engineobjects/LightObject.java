445d22e279b89b63eec811e5ceee7c0f321e609f
Sindri <sindri.weinstein@gmail.com>
2023-11-26 00:47:27 +0100

    Update LightObject with new Effects and removed propertyMap.
    Took 43 minutes

--- core/src/engineobjects/LightObject.java
+++ core/src/engineobjects/LightObject.java
@@ -2,6 +2,7 @@ package engineobjects;
 
 import box2dLight.PointLight;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.mate.engine.MateEngine;
 
 import java.util.Map;
@@ -10,48 +11,88 @@ import java.util.Random;
 public class LightObject {
     private Scene scene;
     private PointLight pointLight;
-    private Map<String, String> propertyMap;
-    private float x, y;
+
+    private Color color;
+    private boolean cast;
+    private float x, y, distance;
 
     public LightObject(Scene scene, Map<String, String> propertyMap) {
         this.scene = scene;
-        this.propertyMap = propertyMap;
+        init(propertyMap);
+    }
+
+    private void init(Map<String, String> propertyMap) {
+        cast = Boolean.parseBoolean(propertyMap.get("cast"));
+        color = MateEngine.convertColor(Long.parseLong(propertyMap.get("color").replace("#", ""), 16));
+
+        if (propertyMap.containsKey("shake"))
+            shake = Boolean.parseBoolean(propertyMap.get("shake"));
+        if (propertyMap.containsKey("wobble"))
+            wobble = Boolean.parseBoolean(propertyMap.get("wobble"));
+        if (propertyMap.containsKey("pulse"))
+            pulse = Boolean.parseBoolean(propertyMap.get("pulse"));
+        if (propertyMap.containsKey("distance"))
+            distance = Float.parseFloat(propertyMap.get("distance"));
     }
 
     public void createStaticLight(float x, float y, float width, float height) {
         this.x = x + width / 2;
         this.y = scene.getSceneHeight() - (y + height / 2);
-        pointLight = new PointLight(scene.getGlobalHandler(), 150, MateEngine.convertColor(Long.parseLong(propertyMap.get("color").replace("#", ""), 16)), width / 2, this.x, this.y);
+        pointLight = new PointLight(scene.getGlobalHandler(), 150, color, width / 2, this.x, this.y);
         pointLight.setStaticLight(true);
         pointLight.setXray(true);
-        if (propertyMap.get("cast").equals("true"))
+        if (cast)
             scene.getPointLights().add(pointLight);
     }
 
     public void createObjectLight(float x, float y) {
         this.x = x;
         this.y = y;
-        pointLight = new PointLight(scene.getGlobalHandler(), 150, MateEngine.convertColor(Long.parseLong(propertyMap.get("color").replace("#", ""), 16)), Float.parseFloat(propertyMap.get("distance")), this.x, this.y);
-        if (propertyMap.get("cast").equals("true"))
+
+        pointLight = new PointLight(scene.getGlobalHandler(), 150, color, distance, this.x, this.y);
+
+        if (cast)
             scene.getPointLights().add(pointLight);
     }
 
+    private boolean shake, wobble, pulse;
     private Random random;
-    private float time;
+    private float timeS, timeW, timeP;
 
     public void update() {
-        time += Gdx.graphics.getDeltaTime();
-        if (propertyMap.get("shake").equals("true")) {
-            if (time > 0.1f) {
-                time = 0;
+        if (shake)
+            if (timeS > 0.1f) {
+                timeS = 0;
                 if (random == null)
                     random = new Random();
                 float x = random.nextFloat(this.x, this.x + 10);
                 float y = random.nextFloat(this.y, this.y + 10);
                 pointLight.setPosition(x, y);
-            }
-        } else
-            pointLight.setPosition(this.x, this.y);
+            } else
+                timeS += Gdx.graphics.getDeltaTime();
+
+        if (wobble)
+            if (timeW > 0.1f) {
+                timeW = 0;
+                if (random == null)
+                    random = new Random();
+                float d = random.nextFloat(distance - 50, distance + 50);
+                pointLight.setDistance(d);
+            } else
+                timeW += Gdx.graphics.getDeltaTime();
+
+        if (pulse)
+            if (timeP > 0.1f) {
+                timeP = 0;
+                //Pulse Code
+            } else
+                timeP += Gdx.graphics.getDeltaTime();
+
     }
 
+    public void setPosition(float x, float y) {
+        this.x = x;
+        this.y = y;
+        pointLight.setPosition(x, y);
+    }
 }
