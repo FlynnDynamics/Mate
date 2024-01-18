ce4aafed493618225510cc653df2f9d375171d24
Flynn <sindri.weinstein@gmail.com>
2024-01-18 21:48:25 +0100

    Webhook Test Commit
    Took 1 minute

--- core/src/scene/Scene.java
+++ core/src/scene/Scene.java
@@ -293,7 +293,7 @@ public class Scene {
 
             if (!nodeName.equals("objectgroup"))
                 continue;
-//sdf
+//sdfsfdsdf
             Map<String, String> layerAttributes = readLayerAttributes(nodeList, i);
             Map<String, String> layerProperties = readLayerProperties(nodeList, i);
 
