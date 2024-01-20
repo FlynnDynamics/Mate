34d494c3fa59dbae4fb821f81b4ff16db087b6c9
Flynn <sindri.weinstein@gmail.com>
2024-01-20 18:15:15 +0100

    [Assets] adding new concept assets.
    Took 49 seconds

---
+++ assets/TileSets/scenetest.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="scenetest" tilewidth="512" tileheight="480" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="15">
+  <image width="512" height="480" source="../res/scenetest/tile000.png"/>
+ </tile>
+ <tile id="16">
+  <image width="512" height="480" source="../res/scenetest/tile001.png"/>
+ </tile>
+ <tile id="17">
+  <image width="512" height="480" source="../res/scenetest/tile002.png"/>
+ </tile>
+ <tile id="18">
+  <image width="512" height="480" source="../res/scenetest/tile003.png"/>
+ </tile>
+ <tile id="19">
+  <image width="512" height="480" source="../res/scenetest/tile004.png"/>
+ </tile>
+ <tile id="20">
+  <image width="512" height="480" source="../res/scenetest/tile005.png"/>
+ </tile>
+ <tile id="21">
+  <image width="512" height="480" source="../res/scenetest/tile006.png"/>
+ </tile>
+ <tile id="22">
+  <image width="512" height="480" source="../res/scenetest/tile007.png"/>
+ </tile>
+ <tile id="23">
+  <image width="512" height="480" source="../res/scenetest/tile008.png"/>
+ </tile>
+ <tile id="24">
+  <image width="512" height="480" source="../res/scenetest/tile009.png"/>
+ </tile>
+ <tile id="25">
+  <image width="512" height="480" source="../res/scenetest/tile010.png"/>
+ </tile>
+ <tile id="26">
+  <image width="512" height="480" source="../res/scenetest/tile011.png"/>
+ </tile>
+ <tile id="27">
+  <image width="512" height="480" source="../res/scenetest/tile012.png"/>
+ </tile>
+ <tile id="28">
+  <image width="512" height="480" source="../res/scenetest/tile013.png"/>
+ </tile>
+ <tile id="29">
+  <image width="512" height="480" source="../res/scenetest/tile014.png"/>
+ </tile>
+</tileset>
---
+++ assets/TileSets/scenetest_2.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="scenetest2" tilewidth="512" tileheight="480" tilecount="7" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="512" height="480" source="../res/scenetest_2/tile005.png"/>
+ </tile>
+ <tile id="1">
+  <image width="512" height="480" source="../res/scenetest_2/tile008.png"/>
+ </tile>
+ <tile id="2">
+  <image width="512" height="480" source="../res/scenetest_2/tile009.png"/>
+ </tile>
+ <tile id="3">
+  <image width="512" height="480" source="../res/scenetest_2/tile010.png"/>
+ </tile>
+ <tile id="4">
+  <image width="512" height="480" source="../res/scenetest_2/tile011.png"/>
+ </tile>
+ <tile id="5">
+  <image width="512" height="480" source="../res/scenetest_2/tile013.png"/>
+ </tile>
+ <tile id="6">
+  <image width="512" height="480" source="../res/scenetest_2/tile014.png"/>
+ </tile>
+</tileset>
